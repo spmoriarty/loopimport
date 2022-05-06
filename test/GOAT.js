c23b3cf2bd99c87be29a63d956e7da89ad0d4571
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-05 17:37:18 -0700

    the GOAT has been populated

--- test/GOAT.js
+++ test/GOAT.js
@@ -3,9 +3,22 @@ export const goat = [
         who: 'The Greatest of all time',
         movie: 'Pig',
         emotion: 'concern',
-        img: './assets/pig'
+        img: './assets/pig',
     },
 
+    {
+        who: 'The Greatest of all time',
+        movie: 'Drive Angry',
+        emotion: 'anger',
+        img: './assets/angr',
+    },
+
+    {
+        who: 'The Greatest of all time',
+        movie: 'National Treasure',
+        emotion: 'speculative',
+        img: './assets/cont',
+    },
 
 
 ];
